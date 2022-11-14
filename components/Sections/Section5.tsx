f895fa999cd732fc41641931a0560e2bf5419048
João Pedro <jp.bruno.machado@gmail.com>
2022-11-14 15:22:24 -0300

    refac

--- components/Sections/Section5.tsx
+++ components/Sections/Section5.tsx
@@ -19,31 +19,31 @@ export default function Section5() {
     {
       title: "E-commerce Website",
       projectUrl: "https://jp-bruno.github.io/ecommerce-website/",
-      previewImg: "./projectsThumbs/ecommerce.png",
+      previewImg: "/ecommerce.png",
       gitUrl: "https://github.com/Jp-bruno/ecommerce-website"
     },
     {
       title: "Space Tourism",
       projectUrl: "https://jp-bruno.github.io/Space-Tourism/",
-      previewImg: "./projectsThumbs/space.png",
+      previewImg: "/space.png",
       gitUrl: "https://github.com/Jp-bruno/Space-Tourism"
     },
     {
       title: "Easy Bank",
       projectUrl: "https://easy-bank-fawn.vercel.app/",
-      previewImg: "./projectsThumbs/easybank.png",
+      previewImg: "/easybank.png",
       gitUrl: "https://github.com/Jp-bruno/EasyBank"
     },
     {
       title: "Task List",
       projectUrl: "https://jp-bruno.github.io/TaskList/",
-      previewImg: "./projectsThumbs/tasklist.png",
+      previewImg: "/tasklist.png",
       gitUrl: "https://github.com/Jp-bruno/TaskList"
     },
     {
       title: "Job List",
       projectUrl: "https://jp-bruno.github.io/JobList/",
-      previewImg: "./projectsThumbs/joblist.png",
+      previewImg: "/joblist.png",
       gitUrl: "https://github.com/Jp-bruno/JobList"
     },
   ];
