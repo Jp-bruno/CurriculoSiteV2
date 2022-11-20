4a6aa3d7d645a342febc81da9d85ae639afd5832
João Pedro <jp.bruno.machado@gmail.com>
2022-11-20 19:04:33 -0300

    refac

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
-import type { AppProps } from 'next/app';
-import AppBar from '../components/AppBar';
-import Footer from '../components/Footer';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import AppBar from "../components/AppBar";
+import Footer from "../components/Footer";
 
-import '../styles/globals.css';
-import '../styles/reset.css';
+import "../styles/globals.css";
+import "../styles/reset.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Front-end Developer</title>
+        <meta property="og:title" content="Front-end Developer" key="title" />
+        <link rel="icon" href="/icons8-react-48.png" />
+        <meta name="description" content="JP Bruno - Front-end Developer" />
+      </Head>
       <AppBar />
       <Component {...pageProps} />
       <Footer />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
