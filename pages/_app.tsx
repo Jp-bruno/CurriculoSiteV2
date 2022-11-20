cdcf432f80305174d44af6c6737e419eff5f9f66
João Pedro <jp.bruno.machado@gmail.com>
2022-11-20 19:29:50 -0300

    teste4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:title" content="Front-end Developer" key="title" />
         <meta property="og:image" content="https://github.com/Jp-bruno/Jp-bruno/blob/master/public/thumbnail.png?raw=true" />
         <meta property="og:type" content="website" />
+        <link rel="image_src" href="https://github.com/Jp-bruno/Jp-bruno/blob/master/public/thumbnail.png?raw=true" />
         <link rel="icon" href="/icons8-react-48.png" />
         <meta name="description" content="JP Bruno - Front-end Developer" />
       </Head>
