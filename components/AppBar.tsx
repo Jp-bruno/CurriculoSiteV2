9a179e0d6f7bdd8231ac98e49b7b675bf000829d
João Pedro <jp.bruno.machado@gmail.com>
2022-12-05 11:57:54 -0300

    removed error line 221

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -218,7 +218,7 @@ export default function Appbar() {
 
   const [languageMenuOpen, setLanguageMenuOpen] = useState(false);
 
-  const { language, changeLanguage } = useContext(LanguageContext.LanguageContext);
+  const { language, changeLanguage } = useContext(LanguageContext);
 
   function toggleMobileMenu() {
     setMobileMenuOpen((prevState) => !prevState);
